d6f8c0cb490a3a42d84a3afafb073379d58b2491
Eric Roué <eric.roue@kyndryl.com>
2024-09-29 10:58:33 +0200

    Ajout msw + actionableStatusCard + StatisticCard + tests

--- src/components/StatisticCard.spec.ts
+++ src/components/StatisticCard.spec.ts
@@ -1,41 +1,41 @@
-import {expect, it, screen, setup} from "@test/utils";
-import StatisticCard from "@/components/StatisticCard.vue";
+import { expect, it, screen, setup } from '@test/utils'
+import StatisticCard from '@/components/StatisticCard.vue'
 
 it('should mount the component', () => {
-    setup(StatisticCard, {
-        renderOptions: {
-            props: {
-                title: "Statistic Card Title",
-                number: 123456789
-            },
-        },
-    })
-    const card = screen.getByTestId('statistic-card')
-    expect(card).toBeInTheDocument()
+  setup(StatisticCard, {
+    renderOptions: {
+      props: {
+        title: 'Statistic Card Title',
+        number: 123456789,
+      },
+    },
+  })
+  const card = screen.getByTestId('statistic-card')
+  expect(card).toBeInTheDocument()
 })
 
 it('should render the title', () => {
-    setup(StatisticCard, {
-        renderOptions: {
-            props: {
-                title: "Statistic Card Title",
-                number: 123456789
-            },
-        },
-    })
-    const titleElement = screen.getByText("Statistic Card Title")
-    expect(titleElement).toBeInTheDocument()
+  setup(StatisticCard, {
+    renderOptions: {
+      props: {
+        title: 'Statistic Card Title',
+        number: 123456789,
+      },
+    },
+  })
+  const titleElement = screen.getByText('Statistic Card Title')
+  expect(titleElement).toBeInTheDocument()
 })
 
 it('should render the number', () => {
-    setup(StatisticCard, {
-        renderOptions: {
-            props: {
-                title: "Statistic Card Title",
-                number: 123456789
-            },
-        },
-    })
-    const numberElement = screen.getByText("123456789")
-    expect(numberElement).toBeInTheDocument()
+  setup(StatisticCard, {
+    renderOptions: {
+      props: {
+        title: 'Statistic Card Title',
+        number: 123456789,
+      },
+    },
+  })
+  const numberElement = screen.getByText('123456789')
+  expect(numberElement).toBeInTheDocument()
 })
