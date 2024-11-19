4bc563ee010f458259b4e4727f5117de93b9382e
Eric Roué <eric.roue@kyndryl.com>
2024-11-19 18:01:29 +0100

    Installation de cypress et playwright installée

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 // register vue composition api globally
-import { ViteSSG } from 'vite-ssg'
+import {ViteSSG} from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
+// import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 import 'uno.css'
 import '@sfxcode/formkit-primevue/dist/sass/formkit-primevue.scss'
-import type { UserModule } from '@/types'
+import type {UserModule} from '@/types'
 import '@/assets/css/primevue-custom.css' // CSS personnalisé, pour les Toasts illisibles dans certains thèmes.
 
 async function prepareApp() {
-  const { worker } = await import('@/mocks/common/browser')
+  const {worker} = await import('@/mocks/common/browser')
   return worker.start()
 }
 
@@ -17,13 +17,13 @@ const routes = setupLayouts(generatedRoutes)
 
 prepareApp().then(() => {
   ViteSSG(
-    App,
-    { routes },
-    (ctx) => {
-      // install all modules under `modules/`
-      Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
-        .forEach(i => i.install?.(ctx))
-    },
+      App,
+      {routes},
+      (ctx) => {
+        // install all modules under `modules/`
+        Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', {eager: true}))
+            .forEach(i => i.install?.(ctx))
+      },
   )
 })
 /* export const createApp = ViteSSG(
